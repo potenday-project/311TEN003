c659625743b587f0543fcf4f74bbf53cfe0eacef
Jungu Lee <1zzangjun@gmail.com>
2023-11-05 03:42:17 +0900

    New : Typography 문서화

---
+++ client/src/stories/Design_system/Typography.stories.tsx
@@ -0,0 +1,89 @@
+import { Typography } from "@mui/material";
+import { Meta, StoryObj } from "@storybook/react";
+const meta = {
+  title: "Design system/Typography",
+  component: Typography,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      options: [
+        "h1",
+        "h2",
+        "subtitle1",
+        "subtitle2",
+        "body",
+        "button",
+        "label",
+        "caption",
+      ],
+      default: "body",
+      control: { type: "select" },
+    },
+  },
+  decorators: [
+    (Story, ctx) => {
+      return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant={ctx.args.variant} sx={{ fontWeight: "bold" }}>
+            {ctx.args.children}
+          </Typography>
+          <Story />
+        </div>
+      );
+    },
+  ],
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Heading1: Story = {
+  args: {
+    variant: "h1",
+    children: "오늘은 이거다 주酒, D-SHOT, DA(alcohol)ily",
+  },
+};
+
+export const Heading2: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "h2",
+  },
+};
+
+export const Subtitle1: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "subtitle1",
+  },
+};
+export const Subtitle2: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "subtitle1",
+  },
+};
+export const Body1: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "body1",
+  },
+};
+export const Button: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "button",
+  },
+};
+export const Label: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "label",
+  },
+};
+export const Caption: Story = {
+  args: {
+    ...Heading1.args,
+    variant: "caption",
+  },
+};
