e2905be3d4f57e3a3af6fdfa40055fd4218a8265
Jungu Lee <1zzangjun@gmail.com>
2023-11-07 04:26:17 +0900

    Minor : Fixme 추가

--- client/src/app/post/[userId]/[postId]/page.tsx
+++ client/src/app/post/[userId]/[postId]/page.tsx
@@ -17,6 +17,7 @@ const PostDetailPage = ({
 }: {
   params: { userId: string; postId: string };
 }) => {
+  // FIXME @로 시작되는 경우만 슬라이스 하도록 추후에 고치고, 함수화 해야함
   const parsedUserId = params.userId.slice(1, params.userId.length);
   const parsedPostId = params.postId;
   return (
