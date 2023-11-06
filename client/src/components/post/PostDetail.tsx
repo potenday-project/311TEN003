eac0758080342b23fefe7988782cc78f5ba7011e
Jungu Lee <1zzangjun@gmail.com>
2023-11-07 04:14:15 +0900

    New : 포스트 디테일 페이지 인터셉트 라우트 구현

---
+++ client/src/components/post/PostDetail.tsx
@@ -0,0 +1,15 @@
+import { PostInterface } from "@/types/post/PostInterface";
+
+const PostDetail = ({
+  image,
+  createdAt,
+  userId,
+  nickname,
+  content,
+  userImage,
+  tags,
+  id,
+}: PostInterface) => {
+  return <>{userId}</>;
+};
+export default PostDetail;
