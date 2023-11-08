927fd2681242b9e51c92857d5f549204a8e9246a
Jungu Lee <1zzangjun@gmail.com>
2023-11-09 04:49:31 +0900

    Feature: React 쿼리 적용 , MSW 통합

--- client/src/components/post/PostDetail.tsx
+++ client/src/components/post/PostDetail.tsx
@@ -1,3 +1,5 @@
+// FIXME 실제 서버연결시 바꿔야함
+"use client";
 import { PostInterface } from "@/types/post/PostInterface";
 import {
   Avatar,
@@ -11,53 +13,59 @@ import {
   Typography,
 } from "@mui/material";
 import PostHashTagList from "./PostHashtagList";
+import { FavoriteBorder, ShareOutlined } from "@mui/icons-material";
+import useGetPostDetailQuery from "@/queries/post/useGetPostDetailQuery";
 
-const PostDetail = ({
-  image,
-  createdAt,
-  userId,
-  nickname,
-  content,
-  userImage,
-  tags,
-  id,
-}: PostInterface) => {
+const PostDetail = () => {
+  const { data } = useGetPostDetailQuery('1');
+  const {
+    postAttachUrl,
+    createdAt,
+    id,
+    nickname,
+    postContent,
+    tagList,
+    likeCount,
+    profileImgUrls,
+  }: PostInterface = data
   return (
     <Card sx={{ p: 4 }}>
       <CardHeader
         avatar={
           <Avatar
             sx={{ bgcolor: "secondary.main" }}
-            src={userImage}
+            src={profileImgUrls}
             data-testid="avatar"
           >
-            {userImage || userId[0].toUpperCase()}
+            {profileImgUrls || id[0].toUpperCase()}
           </Avatar>
         }
-        title={`${userId} ${nickname}`}
+        title={`${id} ${nickname}`}
         subheader={createdAt}
         sx={{ p: 0 }}
       />
       <CardContent sx={{ px: 0 }}>
-        <Typography variant="body1">{content}</Typography>
-        <PostHashTagList tags={tags} />
+        <Typography variant="body1">{postContent}</Typography>
+        <PostHashTagList tags={tagList} />
       </CardContent>
       {/* 이미지 */}
-      {image.length !== 0 && (
+      {postAttachUrl.length !== 0 && (
         <CardMedia
           data-testid="postImg"
           component="img"
           height="360px"
-          image={image[0]}
-          alt={`${userId}의 포스트`}
+          image={postAttachUrl[0]}
+          alt={`${id}의 포스트`}
           sx={{ borderRadius: 2, bgcolor: "background.default" }}
         />
       )}
       <CardActions sx={{ justifyContent: "end", gap: 2, px: 0 }}>
         <ButtonBase data-testid="likeBtn" aria-label="add to favorites">
-          <Typography>좋아요</Typography>
+          <FavoriteBorder />
+          <Typography>{likeCount}</Typography>
         </ButtonBase>
         <ButtonBase data-testid="shareBtn" aria-label="share">
+          <ShareOutlined />
           <Typography>공유하기</Typography>
         </ButtonBase>
       </CardActions>
