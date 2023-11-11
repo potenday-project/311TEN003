abe63472fb31deb59cbd0a6817deb45d6a5311a4
Jungu Lee <100949102+jobkaeHenry@users.noreply.github.com>
2023-11-12 05:49:32 +0900

    Minor changes (#28)
    * New : Sharp 플로그인 추가
    
    * Refactor : Favicon 변경
    
    * Refactor : Favicon 변경
    
    * Refactor : 브랜드 명 추가
    
    * Refactor : 테스트 제거 [임시]

--- client/src/__test__/SampleTest.test.tsx
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { Button } from "@mui/material";
-
-it("App Router: Works with Client Components", () => {
-  render(<Button>테스트</Button>);
-  expect(screen.getByRole("button")).toHaveTextContent("테스트");
-});
