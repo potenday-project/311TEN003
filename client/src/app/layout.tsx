0ec6d776c02179efdd0fef3c87f8ca39c19a77bf
Jungu Lee <1zzangjun@gmail.com>
2023-11-12 05:47:25 +0900

    Refactor : Favicon 변경

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -13,6 +13,9 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 export const metadata: Metadata = {
   title: `${nameOfApp} | ${oneLineMessage}`,
   manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export const viewport: Viewport = {
