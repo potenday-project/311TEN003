a573ecccc608018741526e3bd5e9855295a6d8df
Jungu Lee <1zzangjun@gmail.com>
2023-11-19 20:47:09 +0900

    Minor : 코드컨벤션 개선

--- client/src/components/Navigation/NavbarUserImage.tsx
+++ client/src/components/Navigation/NavbarUserImage.tsx
@@ -3,18 +3,16 @@ import { useUserInfoQuery } from "@/queries/auth/useUserInfoQuery";
 import UserAvatar from "../user/info/UserAvatar";
 import MyIcon from "~/assets/icons/MyIcon.svg";
 const NavbarUserImage = () => {
-  try {
-    const { data } = useUserInfoQuery();
-    return (
-      <UserAvatar
-        sx={{ width: 28, height: 28, m:0.25 }}
-        src={data?.profileImages[0]?.attachUrl}
-        fallback={data?.id}
-      />
-    );
-  } catch (err) {
-    return <MyIcon/>;
-  }
+  const { data } = useUserInfoQuery();
+  return data ? (
+    <UserAvatar
+      sx={{ width: 28, height: 28, m: 0.25 }}
+      src={data?.profileImages[0]?.attachUrl}
+      fallback={data?.id}
+    />
+  ) : (
+    <MyIcon />
+  );
 };
 
 export default NavbarUserImage;
