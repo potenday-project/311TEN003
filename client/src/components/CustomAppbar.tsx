5a3035f22cd3502e5470f67116c9e66601d44a68
Jungu Lee <1zzangjun@gmail.com>
2023-11-19 20:44:32 +0900

    Refactor: 커스텀 앱바 조건부랜더링 추가

--- client/src/components/CustomAppbar.tsx
+++ client/src/components/CustomAppbar.tsx
@@ -1,15 +1,16 @@
-'use client'
+"use client";
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import GoBackIcon from "@/assets/icons/GoBackIcon.svg";
 import { MouseEventHandler, memo } from "react";
 import { useRouter } from "next/navigation";
 
 interface CustomAppbarInterface {
-  title: string;
-  buttonTitle: string;
+  title?: string;
+  buttonTitle?: string;
   disableButton?: boolean;
   onClickButton?: MouseEventHandler<HTMLButtonElement>;
 }
+
 
 const CustomAppbar = ({
   title,
@@ -23,19 +24,21 @@ const CustomAppbar = ({
     <AppBar position={"static"}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <IconButton onClick={() => router.back()}>
-          <GoBackIcon></GoBackIcon>
+          <GoBackIcon/>
         </IconButton>
         <Typography variant="subtitle2" fontWeight={"bold"}>
           {title}
         </Typography>
-        <Button
-          disabled={disableButton}
-          onClick={onClickButton}
-          variant="text"
-          sx={{ minWidth: 40, fontWeight: "medium" }}
-        >
-          {buttonTitle}
-        </Button>
+        {buttonTitle && (
+          <Button
+            disabled={disableButton}
+            onClick={onClickButton}
+            variant="text"
+            sx={{ minWidth: 40, fontWeight: "medium" }}
+          >
+            {buttonTitle}
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
