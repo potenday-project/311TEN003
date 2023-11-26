a3ba0a55d993f7baaaf48d6d9b4925409e61b6b4
Jungu Lee <1zzangjun@gmail.com>
2023-11-27 05:55:40 +0900

    Refactor : 최초 랜더시 쿠키 전송

--- client/src/components/post/PostDetail.tsx
+++ client/src/components/post/PostDetail.tsx
@@ -5,17 +5,21 @@ import PostCard from "./PostCard";
 import { PostInterface } from "@/types/post/PostInterface";
 import { CircularProgress } from "@mui/material";
 import PostCommentList from "@/components/post/PostCommentList";
+import getTokenFromLocalStorage from "@/utils/getTokenFromLocalStorage";
 interface PostDetailInterface {
   postNo: string;
   initialData: PostInterface;
 }
 const PostDetail = ({ postNo, initialData }: PostDetailInterface) => {
-  const { data } = useGetPostDetailQuery(postNo, { initialData });
+  const { data } = useGetPostDetailQuery(postNo, {
+    initialData,
+    headers: { Authorization: getTokenFromLocalStorage() },
+  });
   //FIXME 포스트의 좋아요갯수가 업데이트 되지않음
   return data ? (
     <>
       <PostCard {...data} />
-      <PostCommentList postNo={postNo}/>
+      <PostCommentList postNo={postNo} />
     </>
   ) : (
     <CircularProgress />
