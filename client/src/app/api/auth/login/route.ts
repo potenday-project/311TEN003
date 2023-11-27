3ec3fe40b55cc89b723f0e6bcaa9890658317624
Jungu Lee <1zzangjun@gmail.com>
2023-11-27 10:04:25 +0900

    Fix : CORS 헤더 추가

--- client/src/app/api/auth/login/route.ts
+++ client/src/app/api/auth/login/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: NextRequest) {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Credentials": "true",
         },
       }
     );
@@ -41,6 +42,7 @@ export async function POST(request: NextRequest) {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Credentials": "true",
           },
         }
       );
