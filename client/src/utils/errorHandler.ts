8786f33c781fa412caea5b4e2d7b1c8741474e20
Jungu Lee <1zzangjun@gmail.com>
2023-11-28 02:45:04 +0900

    New : 글로벌팝업 에러핸들링 적용

--- client/src/utils/errorHandler.ts
+++ client/src/utils/errorHandler.ts
@@ -1,12 +1,22 @@
 "use client";
-// import { useGlobalSnackbarStore } from "@/store/useGlobalSnackbarStore";
+import { useGlobalSnackbarStore } from "@/store/useGlobalSnackbarStore";
 import { isAxiosError } from "axios";
+import { useCallback } from "react";
 
-export default function ErrorHandler(error: Error) {
-  // const { fireToast } = useGlobalSnackbarStore();
-  if (isAxiosError(error) && error.response) {
-    // FIXME : Zustand 사용 연구
-    // error.response.status === 401 && fireToast("로그인 후 이용 가능합니다");
-    error.response.status === 401 && console.log("로그인 후 이용 가능합니다");
-  }
-}
+/**
+ * Axios 에러의 status 코드를 판별해 적절한 토스트팝업을 표출해주는 함수를 리런하는 훅
+ * @returns errorHandler (error)=>void
+ */
+export const useErrorHandler = () => {
+  const fireToast = useGlobalSnackbarStore((state) => state.fireToast);
+
+  const errorHandler = useCallback((error: Error) => {
+    if (isAxiosError(error) && error.response) {
+      switch (error.response.status) {
+        case 401:
+          fireToast("로그인 후 이용 가능합니다");
+      }
+    }
+  }, []);
+  return errorHandler;
+};
