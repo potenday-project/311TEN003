f5995d0a30ce11a1d5f45362330eb9ed22b640e2
Jungu Lee <1zzangjun@gmail.com>
2023-11-28 22:07:46 +0900

    Refactor : 술 리스트 컴포넌트, 페이지네이션 분리

--- client/src/app/wiki/page.tsx
+++ client/src/app/wiki/page.tsx
@@ -1,4 +1,4 @@
-import AlcoholList from "@/components/wiki/AlcoholList";
+import AlcoholPagination from "@/components/wiki/AlcoholPagination";
 import WikiAlcoholSelector from "@/components/wiki/WikiAlcoholSelector";
 import { Stack } from "@mui/material";
 import SectionHeading from "@/components/SectionHeading";
@@ -14,7 +14,7 @@ const WikiPage = async () => {
         subTitle={"투파이아들이 쓴 리뷰를 확인할 수 있어요!"}
       />
       <WikiAlcoholSelector />
-      <AlcoholList />
+      <AlcoholPagination />
 
       <SectionHeading title={"술 정보"} subTitle={"곧 출시 됩니다!"} />
       <Stack alignItems="center">
