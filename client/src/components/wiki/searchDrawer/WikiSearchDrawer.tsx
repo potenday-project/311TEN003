6e4268f8d40099e810e8eeb8e151e3b5921156b3
Jungu Lee <1zzangjun@gmail.com>
2023-11-29 22:36:10 +0900

    Refactor : 앱바, 드로워 컴포넌트 분리

--- client/src/components/wiki/searchDrawer/WikiSearchDrawer.tsx
+++ client/src/components/wiki/searchDrawer/WikiSearchDrawer.tsx
@@ -2,46 +2,24 @@ import { SwipeableDrawer, Stack, styled, Box } from "@mui/material";
 import { useContext } from "react";
 import WikiPageContext from "@/store/wiki/WikiPageContext";
 import WikiSerachArea from "@/components/wiki/searchDrawer/WikiSerachArea";
+import CustomSwipeableDrawer from "@/components/CustomSwipeableDrawer";
 
 const WikiSearchDrawer = () => {
   const { isSearching, setIsSearching } = useContext(WikiPageContext);
 
-  const pullerBleed = 24;
+
   return (
-    <SwipeableDrawer
+    <CustomSwipeableDrawer
       open={isSearching}
       onOpen={() => setIsSearching(true)}
       onClose={() => setIsSearching(false)}
-      anchor="bottom"
-      disableSwipeToOpen
-      PaperProps={{
-        sx: {
-          p: 2,
-          borderTopLeftRadius: pullerBleed,
-          borderTopRightRadius: pullerBleed,
-          overFlow: "hidden",
-        },
-      }}
-      ModalProps={{
-        keepMounted: false,
-      }}
     >
-      <Puller />
-      <Stack gap={2} pt={`${pullerBleed + 8}px`}>
+      <Stack gap={2}>
         <WikiSerachArea />
       </Stack>
-    </SwipeableDrawer>
+    </CustomSwipeableDrawer>
   );
 };
 
 export default WikiSearchDrawer;
 
-const Puller = styled(Box)(() => ({
-  width: 56,
-  height: 4,
-  backgroundColor: "#F6EAFB",
-  borderRadius: 3,
-  position: "absolute",
-  top: 8,
-  left: "calc(50% - 28px)",
-}));
