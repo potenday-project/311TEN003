cafff0f83ee228825bc3422eef958234060f38c5
Jungu Lee <1zzangjun@gmail.com>
2023-11-29 22:34:49 +0900

    Refactor : 쿼리 인벨리데이션과정 추가

--- client/src/queries/attach/useNewAttachMutation.ts
+++ client/src/queries/attach/useNewAttachMutation.ts
@@ -1,11 +1,47 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { axiosPrivate } from "@/libs/axios";
 import { ATTACH_FILE, ATTACH_FILE_ResourceType } from "@/const/serverPath";
+import { useErrorHandler } from "./../../utils/errorHandler";
+import { getPostListInfiniteQueryKey } from "./../post/useGetPostListInfiniteQuery";
+import { postDetailQueryKey } from "../post/useGetPostDetailQuery";
+import { MyInfoQueryKeys } from "../auth/useMyInfoQuery";
+import { UserInfoQueryKey } from "../user/useUserInfoQuery";
 
 export const useNewAttachMutation = () => {
+  const errorHandler = useErrorHandler();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (param: { file: File; url: NewAttatchRequestUrl }) =>
       await postImageFn(param.file, param.url),
+    onMutate: (variables) => {
+      return variables;
+    },
+    onSuccess: (_data, variables, context) => {
+      switch (variables.url.type) {
+        case "POST":
+          queryClient.invalidateQueries({
+            queryKey: [getPostListInfiniteQueryKey.all],
+          });
+          queryClient.invalidateQueries({
+            queryKey: [postDetailQueryKey.byId(String(context?.url.pk))],
+          });
+          return;
+        case "PROFILE":
+          queryClient.invalidateQueries({ queryKey: MyInfoQueryKeys.all });
+          queryClient.invalidateQueries({
+            queryKey: UserInfoQueryKey.byId(String(context?.url.pk)),
+          });
+          queryClient.invalidateQueries({
+            queryKey: getPostListInfiniteQueryKey.byKeyword({
+              userNo: String(context?.url.pk),
+            }),
+          });
+        case "ALCOHOL":
+      }
+    },
+    onError: (error) => {
+      errorHandler(error);
+    },
   });
 };
 
