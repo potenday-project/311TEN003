dbaaa988569c016c308dfd5fc0ebf4bf42fafb90
Jungu Lee <1zzangjun@gmail.com>
2023-12-03 13:16:24 +0900

    Refactor : 페이지 디자인 개선

--- client/src/app/post/[userId]/[postId]/page.tsx
+++ client/src/app/post/[userId]/[postId]/page.tsx
@@ -1,8 +1,10 @@
 "use server";
-import PostDetail from "@/components/post/PostDetail";
+import PostDetail from "@/components/post/detail/PostDetail";
 import { getPostDetailQueryFn } from "@/queries/post/useGetPostDetailQuery";
 import getTokenFromCookies from "@/utils/getTokenFromCookies";
+import { Paper, Container } from "@mui/material";
 import { redirect } from "next/navigation";
+import CustomAppbar from "@/components/CustomAppbar";
 
 const PostDetailPage = async ({ params }: { params: { postId: string } }) => {
   const parsedPostId = params.postId;
@@ -15,7 +17,22 @@ const PostDetailPage = async ({ params }: { params: { postId: string } }) => {
   } catch {
     redirect("/not-found");
   }
-  return <PostDetail postNo={parsedPostId} initialData={initialData} />;
+  return (
+    <>
+      <CustomAppbar title={`${initialData.nickname}님의 리뷰`} />
+      <Container sx={{ px: { xs: 0, sm: 4 }, pb: "132px" }} maxWidth={"lg"}>
+        <Paper
+          sx={{
+            display: "flex",
+            position: "relative",
+            flexDirection: "column",
+          }}
+        >
+          <PostDetail postNo={parsedPostId} initialData={initialData} />
+        </Paper>
+      </Container>
+    </>
+  );
 };
 
 export default PostDetailPage;
