355e3057cc43f44174670b09bad703637a0ae251
Jungu Lee <1zzangjun@gmail.com>
2023-12-05 00:19:19 +0900

    Refactor : 회원가입 실패 이유 표출

--- client/src/queries/auth/useSignupMutation.tsx
+++ client/src/queries/auth/useSignupMutation.tsx
@@ -6,11 +6,14 @@ import { useMutation } from "@tanstack/react-query";
 import useLoginMutation from "./useLoginMutation";
 import { useGlobalLoadingStore } from "@/store/useGlobalLoadingStore";
 import { useErrorHandler } from "@/utils/errorHandler";
+import { isAxiosError } from "axios";
+import { useGlobalSnackbarStore } from "@/store/useGlobalSnackbarStore";
 
 const useSignupMutation = () => {
   const { mutate: loginHandler } = useLoginMutation();
   const { setLoading } = useGlobalLoadingStore();
   const errorHandler = useErrorHandler();
+  const fireToast = useGlobalSnackbarStore(({ fireToast }) => fireToast);
 
   return useMutation({
     mutationKey: signupMuataionKey.all,
@@ -30,6 +33,9 @@ const useSignupMutation = () => {
     },
     onError: (err) => {
       errorHandler(err);
+      if (isAxiosError(err) && err.response) {
+        fireToast(err.response.data.detailMessage,'warning');
+      }
     },
   });
 };
