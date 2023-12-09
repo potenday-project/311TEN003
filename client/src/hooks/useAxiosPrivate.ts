8f8fae92078cf589a2da8372dcee99d7c2011527
Jungu Lee <1zzangjun@gmail.com>
2023-12-09 11:47:40 +0900

    Refacotr : 주석추가

--- client/src/hooks/useAxiosPrivate.ts
+++ client/src/hooks/useAxiosPrivate.ts
@@ -1,6 +1,9 @@
 import axios from "@/libs/axios";
 import getTokenFromLocalStorage from "@/utils/getTokenFromLocalStorage";
-
+/**
+ * 헤더에 로컬스토리지에 들어있는 토큰을 싣는 Intercepter 가 달린 Axios
+ * @returns AxiosPrivate
+ */
 const useAxiosPrivate = () => {
   axios.interceptors.request.use(
     (config) => {
