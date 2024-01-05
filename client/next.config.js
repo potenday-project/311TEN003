91488c1dd184b77ee9cbe4982e7b5cea09e3f83f
Jungu Lee <1zzangjun@gmail.com>
2024-01-06 05:55:14 +0900

    New : Next Image remote patterns 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
   webpack(config, { isServer }) {
     // @ts-ignore - rules is a private property that is not typed
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-  
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
       {
@@ -17,22 +27,22 @@ const nextConfig = {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
+        use: ["@svgr/webpack"],
       }
     );
-  
+
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
-  
+
     return config;
   },
-}
+};
 
 //For PWA
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   runtimeCaching: [],
-  sw:'/sw.js',
+  sw: "/sw.js",
 });
-module.exports = withPWA(nextConfig)
+module.exports = withPWA(nextConfig);
