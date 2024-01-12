723e0d73968e4fe8f008089569c3ab644dfb3bc6
Jungu Lee <1zzangjun@gmail.com>
2024-01-12 15:58:42 +0900

    New : 술백과-selector 기능 실제 구현

--- client/src/app/wiki/(wiki-main)/page.tsx
+++ client/src/app/wiki/(wiki-main)/page.tsx
@@ -1,16 +1,28 @@
+"use client";
+import { useState } from "react";
 import AlcoholPagination from "@/components/wiki/AlcoholPagination";
 import WikiAlcoholSelector from "@/components/wiki/WikiAlcoholSelector";
-
 import SectionHeading from "@/components/SectionHeading";
-
+import { Stack } from "@mui/material";
 
 const WikiPage = () => {
+  const [currentAlcoholNo, setCurrentAlcoholNo] = useState<number | undefined>(
+    undefined
+  );
+
   return (
-    <>
-      <SectionHeading title={"술 정보"} subTitle={"다양한 술 정보를 알아보세요!"} />
-      <WikiAlcoholSelector />
-      <AlcoholPagination />
-    </>
+    <Stack gap={2}>
+      <SectionHeading
+        title={"술 정보"}
+        subTitle={"다양한 술 정보를 알아보세요!"}
+      />
+      <WikiAlcoholSelector
+        onChange={(alcoholNo) => {
+          setCurrentAlcoholNo(alcoholNo);
+        }}
+      />
+      <AlcoholPagination alcoholTypeNo={currentAlcoholNo} />
+    </Stack>
   );
 };
 
